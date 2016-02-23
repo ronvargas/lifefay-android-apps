2a75eaecba4dcfe9c3c88c60161313d6c6cc9c63
Daniel Cortes <daniel.cortes@rivetlogic.com>
2016-02-23 11:34:53 -0600

    [ToDos App] Using context to fix bug that was using the wrong URL for the server.

--- LiferayToDos/liferaytodoslibrary/src/main/java/com/rivetlogic/mobile/liferaytodoslibrary/interactor/taskbyuserid/TaskByUserIdInteractorImpl.java
+++ LiferayToDos/liferaytodoslibrary/src/main/java/com/rivetlogic/mobile/liferaytodoslibrary/interactor/taskbyuserid/TaskByUserIdInteractorImpl.java
@@ -1,8 +1,13 @@
 package com.rivetlogic.mobile.liferaytodoslibrary.interactor.taskbyuserid;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 import com.liferay.mobile.android.service.Session;
 import com.liferay.mobile.android.v62.task.TaskService;
 import com.liferay.mobile.screens.base.interactor.BaseRemoteInteractor;
+import com.liferay.mobile.screens.context.LiferayServerContext;
 import com.liferay.mobile.screens.context.SessionContext;
 import com.rivetlogic.mobile.liferaytodoslibrary.interactor.TodosListener;
 
@@ -13,8 +18,11 @@ public class TaskByUserIdInteractorImpl
         extends BaseRemoteInteractor<TodosListener>
         implements TaskByUserIdInteractor {
 
-    public TaskByUserIdInteractorImpl(int screenletId) {
+    private Context context;
+
+    public TaskByUserIdInteractorImpl(int screenletId, Context context) {
         super(screenletId);
+        this.context = context;
     }
 
     @Override
@@ -41,6 +49,10 @@ public class TaskByUserIdInteractorImpl
     }
 
     protected TaskService getTaskService() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        LiferayServerContext.setServer(prefs.getString("liferay_server", ""));
+        LiferayServerContext.setCompanyId(new Long(prefs.getString("company_id", "")));
         Session session = SessionContext.createSessionFromCurrentSession();
         session.setCallback(new TaskByUserIdCallback(getTargetScreenletId()));
         return new TaskService(session);
